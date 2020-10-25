4d20b5aa2ca6dd73b2e59b8a869b7774a5f319d8
castaldo.nello <castaldo.1794724@studenti.uniroma1.it>
2020-10-25 19:00:19 +0100

    final_1

--- scripts.py
+++ scripts.py
@@ -164,7 +164,7 @@ print(result)
 #What's Your Name?
 def print_full_name(a, b):
     print("Hello "+a+" "+b+"! You just delved into python.")
-f __name__ == '__main__':
+if __name__ == '__main__':
 first_name = input()
 last_name = input()
 print_full_name(first_name, last_name)
